cc1b0b43ba1c8d064e7b1e0a33755d1a5592f7b6
Amir Z <Amir.Z.nine.three@gmail.com>
2025-02-24 17:04:57 +0330

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,18 +1,22 @@
-Commodity=[]
-def add_commodity(commodity):
-  Commodity.append(commodity)
+Commodity={}
+def add_commodity(commodity,number):
+  Commodity[commodity]=int(number)
 
 def add():
-  amal=input("if you want add a commodity write(add) or you want see commoditys write(see)")
+  amal=input("if you want add a commodity write(add) or you want see commoditys write(see) if you want buy a commodity write commodity name's:")
   if amal=="add":
     name=input("Enter your commodity name:")
-    add_commodity(name)
+    number=input("Enter your number of commodity:")
+    add_commodity(name,number)
     print("Commodity added!")
     add()
-  elif amal=="see":
-    print(Commodity)
-    add()
+  
   else:
-    print("Can't detect command please try again...")
-    add()
+    if amal in Commodity:
+      if Commodity[amal]>0:
+        print("OK")
+        Commodity[amal]-=1
+      else:
+        print("no OK!")
+        
 add()
